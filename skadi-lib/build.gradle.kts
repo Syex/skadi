08b6ff98328dfc2da1ddf23a22497f54c440f9e8
Tom Seifert <syex@seifertion.de>
2020-05-10 20:25:55 +0200

    Include maven publish stuff in Gradle file

--- skadi-lib/build.gradle.kts
+++ skadi-lib/build.gradle.kts
@@ -1,9 +1,8 @@
 plugins {
     kotlin("jvm")
+    `maven-publish`
+    id("com.jfrog.bintray") version "1.8.5"
 }
-
-group "de.syex"
-version "0.1.0-SNAPSHOT"
 
 repositories {
     // for flow test observer
@@ -20,4 +19,74 @@ dependencies {
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:${Dependencies.coroutines}")
     testImplementation("com.google.truth:truth:${Dependencies.truth}")
     testImplementation("com.github.ologe:flow-test-observer:1.4.0")
+}
+
+val siteUrl = "https://github.com/Syex/skadi"
+val vcsUrl = "https://github.com/Syex/skadi.git"
+val libDescription = "A Kotlin JVM library featuring a redux-like architecture with coroutines"
+
+val libVersion = "0.1.0"
+
+// maven publish plugin
+val sourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("skadi") {
+            group = "de.syex"
+            artifactId = "skadi"
+            version = libVersion
+            from(components["java"])
+            artifact(sourcesJar.get())
+
+            pom {
+                name.set("skadi")
+                description.set(libDescription)
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("Syex")
+                        name.set("Tom Seifert")
+                    }
+                }
+
+                scm {
+                    connection.set(vcsUrl)
+                    developerConnection.set(vcsUrl)
+                    url.set(siteUrl)
+                }
+            }
+        }
+    }
+}
+
+// bintray plugin
+bintray {
+    user = System.getenv("bintrayUser")
+    key = System.getenv("bintrayApiKey")
+    setPublications("skadi")
+    override = true
+    pkg = PackageConfig().apply {
+        repo = "skadi"
+        name = "skadi"
+        setLicenses("Apache-2.0")
+        vcsUrl = vcsUrl
+        publish = true
+        desc = libDescription
+        websiteUrl = siteUrl
+
+        version.apply {
+            name = libVersion
+        }
+    }
 }
